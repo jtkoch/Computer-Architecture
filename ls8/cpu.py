ee5f5d8fbd85d5ae825cfea1e36d73806213299b
jtkoch <jtkoch12@gmail.com>
2020-09-06 14:58:43 -0500

    Day 1

--- ls8/cpu.py
+++ ls8/cpu.py
@@ -7,7 +7,10 @@ class CPU:
 
     def __init__(self):
         """Construct a new CPU."""
-        pass
+        self.pc = 0
+        self.reg = [0] * 8
+        self.ram = [0] * 256
+        self.running = True
 
     def load(self):
         """Load a program into memory."""
@@ -29,6 +32,14 @@ class CPU:
         for instruction in program:
             self.ram[address] = instruction
             address += 1
+
+
+    # new functions for read and write
+    def ram_read(self, index): 
+        return self.ram[index]
+
+    def ram_write(self, index, value):
+        self.ram[index] = value
 
 
     def alu(self, op, reg_a, reg_b):
@@ -62,5 +73,29 @@ class CPU:
 
     def run(self):
         """Run the CPU."""
-        pass
+        
+        HLT = 0b00000001
+        PRN = 0b01000111
+        LDI = 0b10000010
 
+
+        while self.running:
+            instruction = self.ram_read(self.pc)
+            operand_a = self.ram_read(self.pc + 1)
+            operand_b = self.ram_read(self.pc + 2)
+
+            if instruction == HLT:
+                self.running = False
+                self.pc += 1
+
+            elif instruction == PRN:
+                print(operand_a)
+                self.pc += 2
+
+            elif instruction == LDI:
+                self.reg[operand_a] = operand_b
+                self.pc += 3
+
+            else:
+                self.running = False
+                print(f"Bad input: {instruction}")
